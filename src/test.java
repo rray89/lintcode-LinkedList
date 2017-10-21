ece1146035fcb4cae5d32ab635eee4c7e94247b0
rray89 <rray@hotmail.ca>
2017-10-20 23:00:53 -0300

    add insertCyclicSortList

--- src/test.java
+++ src/test.java
@@ -142,4 +142,35 @@ public class test {
 
         return slow;
     }
+
+    /*
+     * http://www.lintcode.com/en/problem/insert-into-a-cyclic-sorted-list/
+     *
+     */
+    public ListNode insertCyclicSortedList(ListNode node, int x) {
+        // Write your code here
+        if (node == null) {
+            node = new ListNode(x);
+            node.next = node;
+            return node;
+        }
+
+        ListNode p = node;
+        ListNode prev = null;
+        do {
+            prev = p;
+            p = p.next;
+            if (x <= p.val && x >= prev.val) {
+                break;
+            }
+            if ((prev.val > p.val) && (x < p.val || x > prev.val)) {
+                break;
+            }
+        } while (p != node);
+
+        ListNode newNode = new ListNode(x);
+        newNode.next = p;
+        prev.next = newNode;
+        return newNode;
+    }
 }
